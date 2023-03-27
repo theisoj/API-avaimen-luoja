4d94737ce0ba9c08cc256aa50c7765a7b412c201
Jesse Keskelä <monkeydie_@hotmail.com>
2023-03-27 05:18:58 +0000

    first commit

---
+++ apiKeyGenerator.js
@@ -0,0 +1,8 @@
+import crypto from "crypto"
+
+const generateApiKey = () => {
+    console.log("\nLuodaan API-avainta...\n")
+    const apiKey = crypto.randomUUID()
+    console.log(`Tässä on API-avain:\n${apiKey}\n`)
+}
+generateApiKey()
